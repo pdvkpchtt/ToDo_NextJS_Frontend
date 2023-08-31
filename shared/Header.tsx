11d0ffefad0aba5ce06dc4b62bb9c629ca110e15
Danil <kabirdanilov14@gmail.com>
2023-08-31 22:40:06 +0500

    done with header

---
+++ shared/Header.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import Image from "@/node_modules/next/image";
+// @ts-ignore
+import Avatar from "react-avatar";
+// @ts-ignore
+import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/24/solid";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl">
+        {/* logo */}
+        <Image
+          src="https://links.papareact.com/c2cdd5"
+          alt="Logo"
+          width={300}
+          height={100}
+          className="w-44 md:w-56 pb-10 md:pb-0 object-contain"
+        />
+
+        <div className="flex items-center space-x-5 flex-1 justify-end w-full">
+          {/* search box */}
+          <form className="flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial">
+            <MagnifyingGlassIcon className="h-6 w-6 text-gray-400" />
+            <input
+              type="text"
+              placeholder="Search"
+              className="flex-1 outline-none p-2"
+            />
+            <button type="submit" hidden>
+              Search
+            </button>
+          </form>
+
+          {/* avatar */}
+          <Avatar name="Tony Montana" round color="#0055d1" size="50" />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
